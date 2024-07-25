700e9e0036d5c0f4ae0c4616d296dda3d3cc8f99
Gurvir <gurvirrandhawa28@gmail.com>
2024-07-25 10:15:37 -0400

    Mapped autoclicker to key held-down

--- pycro-main.py
+++ pycro-main.py
@@ -6,35 +6,25 @@ pyautogui.FAILSAFE = True
 clicking = False
 
 def click_mouse():
-    while True:
+    global clicking
+    while clicking == True:
+        print("running")
         x, y = pyautogui.position()
         pyautogui.doubleClick()
         time.sleep(0.01)
 
 def on_press(key):
-    key = str(key)
-    key = key[1]
-    if key == "a":
+    global clicking
+    if key == key.shift:
         print("true")
         clicking = True
         threading.Thread(target = click_mouse).start()
 
 def on_release(key):
-    key = str(key)
-    key = key[1]
-    if key == "a":
+    global clicking
+    if key == key.shift:
         print("false")
         clicking = False
 
 with keyboard.Listener(on_press = on_press, on_release = on_release) as listener:
     listener.join()
-
-# def on_click(x, y, button, pressed):
-#     print(button)
-#     if button == button.left:
-#         clicking = True
-#         threading.Thread(target = click_mouse).start()
-
-    
-# with mouse.Listener(on_click = on_click) as listener:
-#     listener.join()
