8359f96f94426174ffb397bb6491a6dfebd222ef
Gurvir <gurvirrandhawa28@gmail.com>
2024-07-25 11:28:38 -0400

    Increased click speed

--- pycro-main.py
+++ pycro-main.py
@@ -8,22 +8,17 @@ clicking = False
 def click_mouse():
     global clicking
     while clicking == True:
-        print("running")
-        x, y = pyautogui.position()
-        pyautogui.doubleClick()
-        time.sleep(0.01)
+        pyautogui.click()
 
 def on_press(key):
     global clicking
     if key == key.shift:
-        print("true")
         clicking = True
         threading.Thread(target = click_mouse).start()
 
 def on_release(key):
     global clicking
     if key == key.shift:
-        print("false")
         clicking = False
 
 with keyboard.Listener(on_press = on_press, on_release = on_release) as listener:
